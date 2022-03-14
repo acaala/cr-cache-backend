0b2b08e924c2e8b06a4704c8a29758e146656c3a
Liam Watts <elnewatts@gmail.com>
2022-03-14 11:27:51 +0000

    Added base message

--- routes/cacheRoutes.js
+++ routes/cacheRoutes.js
@@ -3,6 +3,10 @@ const cacheController = require('../controller/cacheController')
 
 const router = express.Router()
 
+
+router.get('/', (req, res) => {
+    res.json('Caching Test');
+})
 router.get('/cachePhotos', cacheController.cache)
 router.get('/clearCachePhotos', cacheController.clearCache)
 
